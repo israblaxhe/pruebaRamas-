86b3628ef338b414421175e9da9b5f416fca7f91
israb <israblaxhe@gmail.com>
2026-01-16 16:42:08 -0600

    HappyFace

--- app/src/main/java/com/example/pruebaramas/MainActivity.kt
+++ app/src/main/java/com/example/pruebaramas/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : ComponentActivity() {
                        Modifier.padding(innerPading)
                    )
 
+                            // =)
+
                    }
 
             }
