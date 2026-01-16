d26f0d000802483d17e6ce6f5909003d85c6db08
israb <israblaxhe@gmail.com>
2026-01-16 13:39:51 -0600

    Login bug Fixed

---
+++ app/src/main/java/com/example/pruebaramas/HomeView.kt
@@ -0,0 +1,39 @@
+package com.example.pruebaramas
+
+import android.net.wifi.hotspot2.pps.HomeSp
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeView(modifier: Modifier) {
+    Column() {
+        Box(
+            Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.Blue)
+
+        )
+
+        Box(
+            Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.Red)
+        )
+
+
+    }
+}
